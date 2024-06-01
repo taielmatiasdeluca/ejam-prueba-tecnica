01686546df4628fc9f02402d2dc7a52aa510dd8e
Taiel <taielmatiasdeluca@gmail.com>
2024-06-01 18:42:01 -0300

    change dinamic routes, make deploy, and add link to live preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://taieldeluca.com.ar/prueba-tecnica/ejam/',
+  base: '/prueba-tecnica/ejam',
+  trailingSlash: "always"
+  
 });
